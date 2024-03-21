2c0b187a169f4ad21533470b22244dcb68f3bdaa
BhavyaBh289 <shahbhavyasp@gmail.com>
2024-03-21 23:59:39 +0530

    added

---
+++ lab_4/index1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sample Form</title>
+</head>
+<body>
+    <h2>Sample Form</h2>
+    <div>
+      <form action="index1.php" method="post">
+          <label for="name">Name:</label>
+          <input type="text" name="name" required><br>
+
+          <label for="email">Email:</label>
+          <input type="email" name="email" required><br>
+
+          <input type="submit" value="Submit">
+      </form>
+    </div>
+</body>
+</html>
